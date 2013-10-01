7b04ecee5a3b6111ef27d6e423a40f6706af7c7c
travstoll <travstoll@gmail.com>
2013-10-01 10:49:18 -0500

    testing procfile

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?
-
-if (function_exists('mysqli_connect')) {
-  //mysqli is installed
-	echo 'it works';
-}
-else{
-die('bad news');
-}  
-
-?>
+<?php 
---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?
+
+if (function_exists('mysqli_connect')) {
+  //mysqli is installed
+	echo 'it works';
+}
+else{
+die('bad news');
+}  
+
+?>
