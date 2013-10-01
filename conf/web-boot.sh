dcc00b00c6a053847dfdbc8efd30961e7e81c127
travstoll <travstoll@gmail.com>
2013-10-01 10:52:38 -0500

    testing procfile

--- conf/web-boot.sh
+++ conf/web-boot.sh
@@ -14,7 +14,7 @@ sed -i 's/Listen 80/Listen '$PORT'/' /app/apache/conf/httpd.conf
 for var in `env|cut -f1 -d=`; do
   echo "PassEnv $var" >> /app/apache/conf/httpd.conf;
 done
-# echo "Include /app/www/conf/httpd/*.conf" >> /app/apache/conf/httpd.conf
+echo "Include /app/www/conf/httpd/*.conf" >> /app/apache/conf/httpd.conf
 touch /app/apache/logs/error_log
 touch /app/apache/logs/access_log
 tail -F /app/apache/logs/error_log &
